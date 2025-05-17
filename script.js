e056e77d3278d9ee6184aa8ebb845e20b5aea19f
Ankita Nag <ankeetaa3@gmail.com>
2025-05-18 00:43:48 +0530

    hero section changed

--- script.js
+++ script.js
@@ -101,3 +101,21 @@ next.addEventListener("click",()=>{
 function rotate(){
     imageContainer.style.transform=`perspective(1300px) rotateY(${x}deg)`;
 };
+const btns=document.querySelectorAll(".nav-btn");
+const slides=document.querySelectorAll(".video-slide");
+var sliderNav=function(manual){
+    btns.forEach((btn)=>{
+        btn.classList.remove("active");
+    });
+    slides.forEach((slide)=>{
+        slide.classList.remove("active");
+    });
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+
+}
+btns.forEach((btn,i)=>{
+    btn.addEventListener("click",()=>{
+        sliderNav(i);
+    })
+})
